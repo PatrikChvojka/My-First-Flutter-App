22cdbd0a4127aceaf0dd7cc0f7c761990694c7c4
Patrik Dvojka <11patrik.sk@gmail.com>
2022-09-17 09:55:22 +0200

    cart 2

--- lib/presentation/pages/cart.dart
+++ lib/presentation/pages/cart.dart
@@ -3,6 +3,7 @@ import 'package:flutter_application_1/presentation/widgets/menu.dart';
 import 'package:flutter_application_1/data/models/food_model.dart';
 // style
 import 'package:flutter_application_1/presentation/style/style.dart' as style;
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class MyCart extends StatefulWidget {
   const MyCart({Key? key}) : super(key: key);
@@ -19,10 +20,32 @@ class _MyCartState extends State<MyCart> {
     myOwnCart.add(MyCartClass(
         imageUrl:
             "https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg",
-        name: "Názov produktu",
-        description: "Toto je popis v košíku",
-        price: "58.50",
-        id: "55"));
+        name: "Názov produktu 1",
+        description: "Toto je popis v košíku 1",
+        price: "18.50",
+        id: "1"));
+
+    myOwnCart.add(MyCartClass(
+        imageUrl:
+            "https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg",
+        name: "Názov produktu 2",
+        description: "Toto je popis v košíku 2",
+        price: "28.50",
+        id: "2"));
+
+    myOwnCart.add(MyCartClass(
+        imageUrl:
+            "https://www.eatthis.com/wp-content/uploads/sites/4/2019/06/deep-dish-pizza-chicago.jpg",
+        name: "Názov produktu 3",
+        description: "Toto je popis v košíku 3",
+        price: "38.50",
+        id: "3"));
+
+    /*
+        myOwnCart.removeWhere((element){
+      return element.id == "3";
+});
+         */
 
     if (myOwnCart != null) {
       // ak mame nieco v kosiku
@@ -31,41 +54,49 @@ class _MyCartState extends State<MyCart> {
         appBar: MainMenu(),
         body: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Container(
-            child: ListView.builder(
+          child: ListView.builder(
               itemCount: myOwnCart.length,
               itemBuilder: (context, index) {
                 // item
                 var item = myOwnCart[index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 8.0, vertical: 2.0),
-                  child: Card(
-                    elevation: 4.0,
-                    child: ListTile(
-                      leading: Image.network(
-                        height: 140.0,
-                        width: 140.0,
-                        item.imageUrl,
-                        fit: BoxFit.cover,
+                return Slidable(
+                  key: Key(myOwnCart[index].id),
+                  // start
+                  startActionPane: ActionPane(
+                    motion: const ScrollMotion(),
+                    //dismissible: DismissiblePane(onDismissed: () {}),
+                    children: const [
+                      SlidableAction(
+                        onPressed: null,
+                        backgroundColor: Color(0xFFFE4A49),
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        label: 'Delete',
                       ),
+                    ],
+                  ),
+                  // end
+                  endActionPane: const ActionPane(
+                    motion: ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        onPressed: null,
+                        backgroundColor: Color(0xFF7BC043),
+                        foregroundColor: Colors.white,
+                        icon: Icons.archive,
+                        label: 'Archive',
+                      ),
+                    ],
+                  ),
+                  // body
+                  child: Card(
+                    child: ListTile(
                       title: Text(item.name),
-                      trailing: GestureDetector(
-                          child: Icon(
-                            Icons.remove_circle,
-                            color: Colors.red,
-                          ),
-                          onTap: () {
-                            setState(() {
-                              //cart.remove(item);
-                            });
-                          }),
+                      subtitle: Text(item.description + "\n" + item.price),
                     ),
                   ),
                 );
-              },
-            ),
-          ),
+              }),
         ),
       );
     } else {
